43ebaace3371e68317d1286471377eb7b3722589
Артур Геращенко <art@realweb.ru>
2019-10-28 19:11:58 +0300

    META-2170 Разработка абстракций работы с Meta EventBus в Python SDK

--- metasdk/event_bus.py
+++ metasdk/event_bus.py
@@ -9,10 +9,19 @@ class Listen(object):
     С него же снимается инфа о том, какие подписки существуют.
     """
 
-    def __init__(self, entity_id: str, code: List[str], form_patterns: List[str] = None):
+    def __init__(self, entity_id: str, code: List[str], form_pattern: List[str] = None, receive_value: bool = False):
+        """
+        Регистрация метода в шине
+        :param entity_id: ID сущности META
+        :param code: Коды ObjectLogService META - ADD, SET, REMOVE
+        :param form_pattern: Формы, на которые долэен реагировать хендлер, может быть пустым. Если пустой - значит form в event-е может быть заполненным или незаполненным,
+                                а если * - то форма долэна быть заполнена, но заполнена любым значением
+        :param receive_value: нужно ли добавлять value в событие
+        """
         self.entity_id = entity_id
         self.code = code
-        self.form_patterns = form_patterns
+        self.form_pattern = form_pattern
+        self.receive_value = receive_value
 
     def __call__(self, original_func):
         get_current_app().event_bus.add_listener(self, original_func)
@@ -43,9 +52,9 @@ class Event(object):
         self.__task = task
 
         edata = task['data']['event']
-        self.user_id = int(edata['user_id'])
-        self.entity_id = str(edata['entity_id'])
-        self.object_id = str(edata['object_id'])
+        self.user_id = int(edata['userId'])
+        self.entity_id = str(edata['entityId'])
+        self.object_id = str(edata['objectId'])
         self.code = str(edata['code'])
         self.form = str(edata['form']) if edata['form'] else None
         self.value = edata.get('value')
@@ -82,7 +91,7 @@ class EventBus(object):
             if event.code not in listener.listen.code:
                 continue
 
-            if not self.__contains_in_form_patterns(event.form, listener.listen.form_patterns):
+            if not self.__contains_in_form_patterns(event.form, listener.listen.form_pattern):
                 continue
 
             listener.handler_func(event)

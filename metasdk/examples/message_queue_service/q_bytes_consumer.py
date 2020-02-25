1b4dc100c29f5675bcd2e71897f3d22f7e4cfa5c
arturgspb <art@realweb.ru>
2020-02-25 21:51:03 +0300

    META-2323 add bytes type

--- metasdk/examples/message_queue_service/q_bytes_consumer.py
+++ metasdk/examples/message_queue_service/q_bytes_consumer.py
@@ -1,10 +1,17 @@
+import os
+
 from metasdk import MetaApp
 
 META = MetaApp()
 q = META.MessageQueueService
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+__DIR__ = os.getcwd()
 
 messages = q.receive_messages("imgs", "g1", serializer="bytes")
 for m in messages:
     print("m = %s" % str(m))
     print("m.value = %s" % str(m.value))
     print("m.value = %s" % str(type(m.value)))
+    with open(__DIR__ + "/copy_metacrm_logo_" + str(m.timestamp) + ".jpg", 'wb') as f:
+        f.write(m.value)
--- metasdk/examples/message_queue_service/q_bytes_producer.py
+++ metasdk/examples/message_queue_service/q_bytes_producer.py
@@ -1,6 +1,16 @@
+import os
+
 from metasdk import MetaApp
 
 META = MetaApp()
 q = META.MessageQueueService
 
-q.send_message("imgs", b'123', serializer="bytes")
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+__DIR__ = os.getcwd()
+
+with open(__DIR__ + "/metacrm_logo.jpg", 'rb') as f:
+    content = f.read()
+    print("content = %s" % str(content))
+    print("content = %s" % str(len(content)))
+    print("content = %s" % str(type(content)))
+    q.send_message("imgs", content, serializer="bytes")

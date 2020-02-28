5be82d535ea910807ff3c7e93e72c1d9c21fc5df
arturgspb <art@realweb.ru>
2020-02-28 23:04:30 +0300

    META-2323 http server

--- metasdk/examples/message_queue_service/ex3_http_stream/http_server.py
+++ metasdk/examples/message_queue_service/ex3_http_stream/http_server.py
@@ -1,3 +1,4 @@
+import base64
 import json
 import os
 from aiohttp import web
@@ -15,25 +16,33 @@ curl -X POST -d 'pb=1&asdasd' 'http://0.0.0.0:9977/collect?gp=1'
 """
 q = META.MessageQueueService
 
+PIXEL_GIF_DATA = base64.b64decode(
+    b"R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7")
 
-async def collect(request: Request):
-    q_data_ = str(request.query_string)
+
+async def write_to_kafka(request: Request):
+    collector_name = request.match_info.get('collector_name')
+
+    q_data_ = collector_name + '|' + str(request.query_string)
     if request.body_exists:
-        post_data = str(await request.text())
+        post_data = str(request.text())
         if post_data:
             if q_data_:
                 q_data_ += "&"
             q_data_ += post_data
-
     q.send_message("collect", q_data_.encode(), serializer="bytes")
 
-    return web.Response()
+
+async def collect(request: Request):
+    request.loop.create_task(write_to_kafka(request))
+
+    return web.Response(body=PIXEL_GIF_DATA, content_type='image/gif')
 
 
 app = web.Application()
 
-app.router.add_get("/collect", collect)
-app.router.add_post("/collect", collect)
+app.router.add_get("/collect/{collector_name}", collect)
+app.router.add_post("/collect/{collector_name}", collect)
 
 host = "0.0.0.0"
 if is_prod:
@@ -42,4 +51,4 @@ else:
     port = 9977
 
 if __name__ == "__main__":
-    web.run_app(app, host=host, port=port, access_log=False)
+    web.run_app(app, host=host, port=port)

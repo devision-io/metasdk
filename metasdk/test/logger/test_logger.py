d62a17ce538135ad909df66b31923787f27951e5
Artem Markov <a.markov@realweb.ru>
2020-08-12 10:43:13 +0300

    META-2524 [+] Add tests for work with log entity on error log level

--- metasdk/test/logger/test_logger.py
+++ metasdk/test/logger/test_logger.py
@@ -3,6 +3,7 @@ import logging
 from _pytest.logging import LogCaptureFixture
 
 from metasdk import MetaApp
+from metasdk.logger import LOGGER_ENTITY
 
 log = MetaApp().log
 
@@ -18,42 +19,46 @@ def assert_and_clear_caplog(caplog: LogCaptureFixture, asserted_msg: str, assert
 
 
 def test_log_entity(caplog):
-    #TODO Refactor it (automate duplicated parts)
-    #TODO Add test for error log level
+    """Test work with LOG_ENTITY on different log levels"""
+
     with caplog.at_level(logging.INFO):
-        # Try log with context, without some set entities
-        log.info("info_message_0", {"info_context_key_0": "info_context_value_0"})
-        assert_and_clear_caplog(caplog, "info_message_0", {"info_context_key_0": "info_context_value_0"})
+        for log_level in [log.info, log.error]:
+            # Try log with context, without some set entities
+            log_level("info_message_0", {"info_context_key_0": "info_context_value_0"})
+            assert_and_clear_caplog(caplog, "info_message_0", {"info_context_key_0": "info_context_value_0"})
 
-        # ...with one set entity
-        log.set_entity("entity_key_1", "entity_value_1")
-        log.info('info_message_1', {"info_context_key_1": "info_context_value_1"})
-        assert_and_clear_caplog(caplog,
-                                "info_message_1",
-                                {"info_context_key_1": "info_context_value_1",
-                                 "entity_key_1": "entity_value_1"})
+            # ...with one set entity
+            log.set_entity("entity_key_1", "entity_value_1")
+            log_level('info_message_1', {"info_context_key_1": "info_context_value_1"})
+            assert_and_clear_caplog(caplog,
+                                    "info_message_1",
+                                    {"info_context_key_1": "info_context_value_1",
+                                     "entity_key_1": "entity_value_1"})
 
-        # ...change this entity value
-        log.set_entity("entity_key_1", "entity_value_2")
-        log.info('info_message_2', {"info_context_key_2": "info_context_value_2"})
-        assert_and_clear_caplog(caplog,
-                                "info_message_2",
-                                {"info_context_key_2": "info_context_value_2",
-                                 "entity_key_1": "entity_value_2"})
+            # ...change this entity value
+            log.set_entity("entity_key_1", "entity_value_2")
+            log_level('info_message_2', {"info_context_key_2": "info_context_value_2"})
+            assert_and_clear_caplog(caplog,
+                                    "info_message_2",
+                                    {"info_context_key_2": "info_context_value_2",
+                                     "entity_key_1": "entity_value_2"})
 
-        # ...add one another entity
-        log.set_entity("entity_key_3", "entity_value_3")
-        log.info('info_message_3', {"info_context_key_3": "info_context_value_3"})
-        assert_and_clear_caplog(caplog,
-                                "info_message_3",
-                                {"info_context_key_3": "info_context_value_3",
-                                 "entity_key_1": "entity_value_2",
-                                 "entity_key_3": "entity_value_3"})
+            # ...add one another entity
+            log.set_entity("entity_key_3", "entity_value_3")
+            log_level('info_message_3', {"info_context_key_3": "info_context_value_3"})
+            assert_and_clear_caplog(caplog,
+                                    "info_message_3",
+                                    {"info_context_key_3": "info_context_value_3",
+                                     "entity_key_1": "entity_value_2",
+                                     "entity_key_3": "entity_value_3"})
 
-        # ...remove first entity
-        log.remove_entity("entity_key_1")
-        log.info('info_message_4', {"info_context_key_4": "info_context_value_4"})
-        assert_and_clear_caplog(caplog,
-                                "info_message_4",
-                                {"info_context_key_4": "info_context_value_4",
-                                 "entity_key_3": "entity_value_3"})
+            # ...remove first entity
+            log.remove_entity("entity_key_1")
+            log_level('info_message_4', {"info_context_key_4": "info_context_value_4"})
+            assert_and_clear_caplog(caplog,
+                                    "info_message_4",
+                                    {"info_context_key_4": "info_context_value_4",
+                                     "entity_key_3": "entity_value_3"})
+
+            # clear for next iteration with another log level
+            log.remove_entity("entity_key_3")
